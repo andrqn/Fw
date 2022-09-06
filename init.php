6ba9540f390a798e8fcd278e335bd73769f15e56
Dzmitry <78901012+andrqn@users.noreply.github.com>
2022-09-06 18:43:13 +0300

    Update init.php

--- init.php
+++ init.php
@@ -3,7 +3,6 @@
 
 
 spl_autoload_register(function (string $class) {
-    echo "try load $class" . PHP_EOL;
     $path = __DIR__ . "/../{$class}" . ".php";
     if (file_exists($path)) {
         require_once $path;
