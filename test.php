e1f02b1defbb31430ed2de8234ec334c1b014957
andrqn <andreikindmtry@gmail.com>
2022-09-21 19:07:03 +0300

    update

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-include ('init.php');
-$app->pager->addCss('style.css');
-$app->pager->addJs('scripts.js');
-$app->pager->addString('<h1></h1>');
-
-print_r($app->pager->getAllReplace());
-$app->pager->showHead();
