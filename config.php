f636f40c0693f0eed2c6c00fc1e6db5c6c10d7d6
andrqn <andreikindmtry@gmail.com>
2022-09-21 16:26:18 +0300

    Revert "Revert "Step Two""
    This reverts commit 30ea84b640b4057f9d7a467155668454c1d3ce99.

--- config.php
+++ config.php
@@ -1,5 +1,17 @@
 <?php
 
+<<<<<<< Updated upstream
 $config["db"] = "db";
 $config["login"] = "login";
 $config["password"] = "password";
+=======
+return [
+    "db" => [
+        "login" => 'root',
+        "password" => '123'
+    ],
+    'template' => [
+        'news'
+    ]
+];
+>>>>>>> Stashed changes
