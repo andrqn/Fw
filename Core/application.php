30ea84b640b4057f9d7a467155668454c1d3ce99
Dzmitry <andreikindmtry@gmail.com>
2022-09-21 16:02:24 +0300

    Revert "Step Two"
    This reverts commit 0abe34f231a8a7866ed64765b73dd93ad93aa898.

--- Core/application.php
+++ Core/application.php
@@ -2,61 +2,25 @@
 
 namespace Fw\core;
 
-use traits\Singletone;
-
-
 class Application
 {
-<<<<<<< Updated upstream:Core/application.php
     /**
      * @var null
      */
     private static $instance = null;
-=======
-    use Singletone;
-
->>>>>>> Stashed changes:core/Application.php
     private $__components = [];
-    public Page $pager ;
+    private $pager = null;
     private $template = null;
 
     private function __construct()
     {
-        $this->pager = Page::getInstance();
-        $this->template = $_SERVER['DOCUMENT_ROOT'].'/template/';
-
-
     }
 
-    public function startBuffer()
+    public function getInstance()
     {
-        ob_start();
+        if (!Application::$instance) {
+            self::$instance = new self();
+        }
+        return self::$instance;
     }
-
-    public function endBuffer()
-    {
-        ob_end_clean();
-    }
-
-    public function restartBuffer()
-    {
-        ob_clean();
-    }
-<<<<<<< Updated upstream:Core/application.php
-=======
-
-    public function header()
-    {
-        $this->startBuffer();
-        require $this->template . 'news/header.php';
-    }
-
-
-    public function footer()
-    {
-        require $this->template . 'news/footer.php';
-
-    }
-
->>>>>>> Stashed changes:core/Application.php
 }
--- config.php
+++ config.php
@@ -1,17 +1,5 @@
 <?php
 
-<<<<<<< Updated upstream
 $config["db"] = "db";
 $config["login"] = "login";
 $config["password"] = "password";
-=======
-return [
-    "db" => [
-        "login" => 'root',
-        "password" => '123'
-    ],
-    'template' => [
-        'news'
-    ]
-];
->>>>>>> Stashed changes
